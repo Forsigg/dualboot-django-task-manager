66477ec3a30c96ab4ae4077aaa1357bf87e6cdc0
forsigg <al.berhmiller@gmail.com>
2022-10-20 18:02:18 +0600

    В TaskSerializer добавлено поле tags, которое содержит вложенный TagSerializer

--- main/serializers.py
+++ main/serializers.py
@@ -6,17 +6,38 @@ from main.models import User, Task, Tag
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_of_birth', 'phone')
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ('id', 'title', 'description', 'created_at', 'edited_at', 'deadline',
-                  'priority', 'author', 'executor', 'get_tags', 'state')
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "date_of_birth",
+            "phone",
+        )
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('id', 'title')
+        fields = ("id", "title")
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Task
+        fields = (
+            "id",
+            "title",
+            "description",
+            "created_at",
+            "edited_at",
+            "deadline",
+            "priority",
+            "author",
+            "executor",
+            "tags",
+            "state",
+        )
